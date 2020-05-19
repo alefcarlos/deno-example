1a86a6c66d690c9ae21f24ce6c8f66f6e527c8c6
Alef Carlos <alef.carlos@gmail.com>
2020-05-19 16:30:59 -0300

    docs: example

--- sum.ts
+++ sum.ts
@@ -2,6 +2,10 @@
  * Sum values
  * @param  {number[]} ...args - Values
  * @returns number - Sum of values
+ * 
+ * Example:
+ *
+ *     const result = add(1, 2, 3);
  */
 export function add(...args: number[]): number {
   return args.reduce((previous, current) => previous + current);
